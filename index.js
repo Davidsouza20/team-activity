05992e64e6cc9792f0aa72bb67ce09be91bc8a34
David Souza/LGESP RBrisatc654.souzaD MC PVG(brisatc654.souza@lgepartner.com) <brisatc654.souza@lge.com>
2019-05-21 13:48:52 -0300

    Organizing the code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function search() {
     var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
-    console.log(responseText);
+    console.log(this.responseText);
    document.getElementById("demo").innerHTML = this.responseText;
   }
 };
