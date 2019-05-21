84d9553aa1ae6da9c11a32fc1aa8e36b5b07cb0e
David Souza/LGESP RBrisatc654.souzaD MC PVG(brisatc654.souza@lgepartner.com) <brisatc654.souza@lge.com>
2019-05-21 11:44:59 -0300

    Organizing the code

--- index.php
+++ index.php
@@ -2,27 +2,7 @@
 
 echo '<h1>Scripture Resources</h1>';
 
-try
-{
-  $dbUrl = getenv('DATABASE_URL');
-
-  $dbOpts = parse_url($dbUrl);
-
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
-
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
+include("dbconection.php");
 
 foreach ($db->query("SELECT * FROM scriptures_table") as $row) {
     echo '<p>';
@@ -37,3 +17,19 @@ foreach ($db->query("SELECT * FROM scriptures_table") as $row) {
 }
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <form action="">
+    Book: <input type="text" name="book"><br>
+  </form>
+</body>
+</html>
