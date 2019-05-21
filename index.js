0eba97d5688b636fac62ab3e39a00eafcfd48be3
David Souza/LGESP RBrisatc654.souzaD MC PVG(brisatc654.souza@lgepartner.com) <brisatc654.souza@lge.com>
2019-05-21 13:59:50 -0300

    database conection test

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 function search() {
     var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "search.php", true);
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     console.log(this.responseText);
    document.getElementById("demo").innerHTML = this.responseText;
   }
 };
-xhttp.open("GET", "search.php", true);
+
 xhttp.send();
 }
   
