24a6e8647a472cc00bed83c445799a57e4ecc76c
David Souza/LGESP RBrisatc654.souzaD MC PVG(brisatc654.souza@lgepartner.com) <brisatc654.souza@lge.com>
2019-05-21 15:16:12 -0300

    database conection test

--- index.php
+++ index.php
@@ -25,8 +25,8 @@ $book = "john";
 
    
 <?php
-
-foreach ($db->query("SELECT * FROM scriptures_table") as $row) {
+$book = "john";
+foreach ($db->query("SELECT * FROM scriptures_table WHERE book=$book") as $row) {
     echo '<p>';
     echo '<strong>' . $row['book'] . '</strong>' . '&nbsp;';
 
