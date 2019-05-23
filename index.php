e1b9a6a1bab69d24e600fc8cbfb52b1f896049bc
David Souza <david.jsouzaa@gmail.com>
2019-05-22 22:05:51 -0300

    start stretch 2

--- index.php
+++ index.php
@@ -8,10 +8,15 @@ include("dbconection.php");
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
   <title>Document</title>
 </head>
 <body>
-    <form action="" method="post">
+    <form class="form-group" action="" method="post">
     Book: <input type="text" name="book"><br>
     <input class="btn btn-success" type="submit" value="Search">
     </form>
@@ -40,7 +45,7 @@ foreach ($db->query($query) as $row) {
     echo '<strong>' . $row['verse'] . '</strong>' . '&nbsp;' . '-';
 
     //echo '"' . $row['content'] . '"';
-    echo '</a>';
+    echo '</a><br>';
 }
 
 
