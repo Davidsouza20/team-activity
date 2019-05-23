8b913b63d86df6fd3a31ca3cd14e9cf79c28c46a
David Souza <david.jsouzaa@gmail.com>
2019-05-22 22:29:14 -0300

    start stretch 2

--- details.php
+++ details.php
@@ -1,7 +1,19 @@
 <?php 
 $query = "SELECT * 
-            FROM scriptures_table 
-            WHERE LOWER(book)=" ."'" . LOWER($book) ."'";
+FROM scriptures_table 
+WHERE LOWER(book)=" ."LOWER('" . $book ."')";
+
+foreach ($db->query($query) as $row) {
+echo '<a class="m-3" href="details.php">';
+echo '<strong>' . $row['book'] . '</strong>' . '&nbsp;';
+
+echo '<strong>' . $row['chapter'] . '</strong>' . ':';
+
+echo '<strong>' . $row['verse'] . '</strong>' . '&nbsp;' . '-';
+
+//echo '"' . $row['content'] . '"';
+echo '</a><br>';
+}
 
 
 ?>
