b0c53d4be50d970c7f8ce6c826142e60d78013fc
David Souza <david.jsouzaa@gmail.com>
2019-05-22 23:35:31 -0300

    start stretch 3

--- details.php
+++ details.php
@@ -1,5 +1,23 @@
 <?php 
     session_start();
-    echo $_SESSION['content'];
+    //echo $_SESSION['content'];
+    $id = $_SESSION['content'];
+
+    $query = "SELECT * 
+            FROM scriptures_table 
+            WHERE id= $id";
+
+echo "<h1>Scripture Resources</h1>";
+foreach ($db->query($query) as $row) {
+    echo '<p class="m-3" href="details.php">';
+    echo '<strong>' . $row['book'] . '</strong>' . '&nbsp;';
+
+    echo '<strong>' . $row['chapter'] . '</strong>' . ':';
+    
+    echo '<strong>' . $row['verse'] . '</strong>' . '&nbsp;' . '-';
+  
+    echo '"' . $row['content'] . '"';
+    echo '</p><br>';
+}
 
 ?>
