7386606946513236d3ec4726c9104f7cabb59459
David Souza/LGESP RBrisatc654.souzaD MC SW(brisatc654.souza@lgepartner.com) <brisatc654.souza@lge.com>
2019-05-28 18:26:38 -0300

    Core requirements step 2

--- scripture-list.php
+++ scripture-list.php
@@ -6,7 +6,7 @@ $verse = $_POST['verse'];
 $content = $_POST['content'];
 
 
-$query = "INSERT INTO scriptures_table ('book', 'chapter', 'verse') VALUES (''$book'', ''$chapter'', ''$verse'', ''$content''')"; 
+$query = "INSERT INTO scriptures_table ('book', 'chapter', 'verse') VALUES ('$book', '$chapter', '$verse', '$content')"; 
 
 echo $query;
 
