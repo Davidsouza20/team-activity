3829d4507e540002d7a3d7c6e50d00c5c90d0d24
David Souza <david.jsouzaa@gmail.com>
2019-05-29 23:39:07 -0300

    handle insert data to database

--- scripture-list.php
+++ scripture-list.php
@@ -4,15 +4,16 @@ $book = $_POST['book'];
 $chapter = $_POST['chapter'];
 $verse = $_POST['verse'];
 $content = $_POST['content'];
-$query = "INSERT INTO scriptures_table ('book', 'chapter', 'verse') VALUES ('$book', '$chapter', '$verse', '$content')"; 
-$result = pg_query($db, $query);
-if(!$result) {
-    echo pg_last_error($db);
- } else {
-    echo "Records created successfully\n";
- }
- pg_close($db);
-foreach ($_POST['topic'] as $topic) {
-    echo $topic;
-}
+
+$query = 'INSERT INTO scriptures_table (book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content)'; 
+      
+      
+$stmt = $db->prepare($query);
+$stmt->bindValue(':book', $book, PDO::PARAM_STR);
+$stmt->bindValue(':chapter', $chapter, PDO::PARAM_INT);
+$stmt->bindValue(':verse', $verse, PDO::PARAM_INT);
+$stmt->bindValue(':content', $content, PDO::PARAM_STR);
+$stmt->execute();
+die();
+
 ?>
