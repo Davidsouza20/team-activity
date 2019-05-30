0e171f707d5af384ab1183119b5aae0ee5c8c153
David Souza <david.jsouzaa@gmail.com>
2019-05-29 23:20:30 -0300

    handle insert data to database

--- scripture-list.php
+++ scripture-list.php
@@ -6,20 +6,20 @@ $verse = $_POST['verse'];
 $content = $_POST['content'];
 
 
-$query = "INSERT INTO scriptures_table (book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content)"; 
+$query = 'INSERT INTO scriptures_table (book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content)'; 
       
       
 $stmt = $db->prepare($query);
 $stmt->bindValue(':book', $book, PDO::PARAM_STR);
-$stmt->bindValue(':chapter', $chapter, PDO::PARAM_STR);
-$stmt->bindValue(':verse', $verse, PDO::PARAM_STR);
+$stmt->bindValue(':chapter', $chapter, PDO::PARAM_INT);
+$stmt->bindValue(':verse', $verse, PDO::PARAM_INT);
 $stmt->bindValue(':content', $content, PDO::PARAM_STR);
 $stmt->execute();
 
 die();
 
 
-foreach
+
 
 
 ?>
